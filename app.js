f5581b9792b97ff55d4aa68c49c9d85a72346c3a
Khadija <khadija.k.qaraqe@gmail.com>
2016-11-01 11:48:47 +0200

    updates

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 (function () {
-//'';
-  angular.module('module1App', [])
-.controller('module1Cont',
-function ($scope) {
+	'use strict';
 
-});
+	angular.module('LunchCheck',[]).controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject =['$scope'];
+
+	function LunchCheckController ($scope){
+		$scope.dishes = '';
+		$scope.txtMsg = '';
+		$scope.fontStyle = {};
+		$scope.boxStyle = {};
+
+		$scope.checkItems = function () {
+
+			if($scope.dishes !== '') {
+				var count = $scope.dishes.split(',').length;
+				if(count > 0){
+					if(count <=3){
+						$scope.txtMsg = 'Enjoy your Dishes';
+					}
+					else if(count > 3){
+						$scope.txtMsg = 'Too much Dishes'
+					}
+					$scope.fontStyle = {
+						"color":"green"
+					};
+					$scope.boxStyle = {
+						"border-color":"green"
+					};
+				}
+				else {
+					enterItems();
+				}
+			}
+			else {
+				enterItems();
+			}
+		}
+		var enterItems = function() {
+			$scope.txtMsg = 'Please enter data first';
+				$scope.fontStyle = {
+					"color":"red"
+				};
+				$scope.boxStyle = {
+					"border-color":"red"
+				};
+		};
+	}
 })();
